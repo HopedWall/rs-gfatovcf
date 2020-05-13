684691dd75a2451e8191a490dfe885c3b1a2d77e
HopedWall <francesco.porto97@gmail.com>
2020-05-13 19:13:33 +0200

    Some tests

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,23 @@ fn main() {
             let num_paths = g.paths.len();
             let num_conts = g.containments.len();
 
-            println!("num_segs: {}",num_segs);
-            println!("num_links: {}",num_links);
-            println!("num_paths: {}",num_paths);
-            println!("num_conts: {}",num_conts);
+            //println!("num_segs: {}",num_segs);
+            //println!("num_links: {}",num_links);
+            //println!("num_paths: {}",num_paths);
+            //println!("num_conts: {}",num_conts);
+
+            println!("Segments");
+            for seg in &g.segments {
+                println!("{}",seg.name);
+            }
+            println!("Links");
+            for link in &g.links {
+                println!("{}",link.from_segment);
+            }
+            //println!("segs: {}",g.segments);
+            //println!("links: {}",num_links);
+            //println!("paths: {}",num_paths);
+            //println!("conts: {}",num_conts);
         }
     }
 }
