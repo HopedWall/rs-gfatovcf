ebe229477762afeeddc98f93331c2fdef7320003
Francesco Porto <francesco.porto97@gmail.com>
2020-05-18 14:57:51 +0200

    First graph attempt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::path::PathBuf;
 use petgraph::Graph;
 use petgraph::dot::Dot;
+// for the graph
+use petgraph::dot::Config;
+use petgraph::graphmap::DiGraphMap;
 
 fn main() {
     
@@ -18,30 +21,34 @@ fn main() {
         None => panic!("Error parsing GFA file"),
         Some(g) => {
 
-            let mut deps = Graph::<&str,()>::new();
-            
+            //let mut graph = Graph::<(&str, &str),_>::new();
+            //let mut graph = Graph::<&str,&str>::new();
+
             println!("Segments");
             for seg in &g.segments {
-                println!("{},{}",seg.name,seg.sequence);
+                //println!("{},{}",seg.name,seg.sequence);
+                //let t = (&seg.name, &seg.sequence);
+                //graph.add_node(&seg.name);
+                
             }
 
             println!("Links");
+            let mut edges: Vec<(&str, &str)> = Vec::new();
             for link in &g.links {
-                println!("{},{},{},{},{}",link.from_segment, link.from_orient.as_bool(), link.to_segment, link.to_orient.as_bool(), link.overlap);
+                //println!("{},{},{},{},{}",link.from_segment, link.from_orient.as_bool(), link.to_segment, link.to_orient.as_bool(), link.overlap);
+                //graph.add_edge(&link.from_segment, &link.to_segment, "0");
+                edges.push((&link.from_segment, &link.to_segment))
             }
+            let g = DiGraphMap::<&str, &str>::from_edges(edges);
+            let x = Dot::with_config(&g, &[Config::EdgeIndexLabel]);
+            println!("{}", x);
 
-            println!("Paths");
-            for path in &g.paths {
-                println!("{},{:?},{:?}", path.path_name, path.segment_names, path.overlaps);
-            }
-            
-            // println("Containments");
-            // for c in &g.containments {
-            //     println!("")
+            // println!("Paths");
+            // for path in &g.paths {
+            //     println!("{},{:?},{:?}", path.path_name, path.segment_names, path.overlaps);
             // }
 
-
-            //println!("{}", Dot::new(&deps));
+            //println!("{}", Dot::new(&graph));
 
         }
     }
