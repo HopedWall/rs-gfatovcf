33903d5526f4869db858d7db656381330c965973
Francesco Porto <francesco.porto97@gmail.com>
2020-05-23 12:15:23 +0200

    Added parse reference, graph almost working

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 use std::path::PathBuf;
-use handlegraph::graph::HashGraph;
-use handlegraph::handlegraph::HandleGraph;
-use handlegraph::handle::NodeId;
+//use handlegraph::graph::HashGraph;
+//use handlegraph::handlegraph::HandleGraph;
+//use handlegraph::handle::NodeId;
 use gfa::gfa::GFA;
 use std::fs::File;
 use std::io::Write;
 extern crate clap;
-use clap::{Arg, App};
+//use clap::{Arg, App};
 extern crate chrono;
 use chrono::Utc;
+use std::io::{BufReader,BufRead};
+use petgraph::graph::Graph;
+use petgraph::dot::{Dot, Config};
+use petgraph::graphmap::DiGraphMap;
+use std::collections::HashMap;
 
 struct Variant {
     chromosome: String,
@@ -23,50 +28,94 @@ struct Variant {
 
 fn main() {
 
-    let matches = App::new("rs-GFAtoVCF")
-                          .version("1.0")
-                          .author("Francesco Porto <francesco.porto97@gmail.com>")
-                          .about("Converts GFA to VCF")
-                          .arg(Arg::with_name("input")
-                               .short("i")
-                               .long("input")
-                               .value_name("FILE")
-                               .help("Sets the input file to use")
-                               .required(true)
-                               .takes_value(true))
-                          .arg(Arg::with_name("output")
-                               .short("o")
-                               .long("output")
-                               .value_name("FILE")
-                               .help("Sets the output file to use")
-                               .required(true)
-                               .takes_value(true))
-                          .get_matches();
+    // let matches = App::new("rs-GFAtoVCF")
+    //                       .version("1.0")
+    //                       .author("Francesco Porto <francesco.porto97@gmail.com>")
+    //                       .about("Converts GFA to VCF")
+    //                       .arg(Arg::with_name("reference")
+    //                            .short("r")
+    //                            .long("reference")
+    //                            .value_name("FILE")
+    //                            .help("Sets the reference file to use")
+    //                            .required(true)
+    //                            .takes_value(true))
+    //                       .arg(Arg::with_name("input")
+    //                            .short("i")
+    //                            .long("input")
+    //                            .value_name("FILE")
+    //                            .help("Sets the input file to use")
+    //                            .required(true)
+    //                            .takes_value(true))
+    //                       .arg(Arg::with_name("output")
+    //                            .short("o")
+    //                            .long("output")
+    //                            .value_name("FILE")
+    //                            .help("Sets the output file to use")
+    //                            .required(true)
+    //                            .takes_value(true))
+    //                       .get_matches();
 
-    let in_path = matches.value_of("input").expect("Could not parse argument --input");
-    let out_path = matches.value_of("output").expect("Could not parse argument --output");
+    // let in_path = matches.value_of("input").expect("Could not parse argument --input");
+    // let out_path = matches.value_of("output").expect("Could not parse argument --output");
 
-    //let in_path = "./input/samplePath3.gfa";
-    //let out_path = "./input/samplePath3.vcf";
+    let ref_path = "./input/samplePath3.fa";
+    let in_path = "./input/samplePath3.gfa";
+    let out_path = "./input/samplePath3.vcf";
 
-    if let Some(gfa) = gfa::parser::parse_gfa(&PathBuf::from(in_path)) {
-        let graph = HashGraph::from_gfa(&gfa);
+    if let Some(reference) = parse_reference(&PathBuf::from(in_path)) {
+        
+        if let Some(gfa) = gfa::parser::parse_gfa(&PathBuf::from(in_path)) {
+            //let graph = HashGraph::from_gfa(&gfa);
+            //println!("{:?}",graph);
+            
+            //Build graph from scratch
+            let graph = build_graph(&gfa);
 
-        println!("{:?}",graph);
-        let variations = find_variants(&graph);
-        write_to_file(&PathBuf::from(out_path), &variations).unwrap();
+            //Print graph
+            let x = Dot::with_config(&graph, &[Config::EdgeIndexLabel]);
+            println!("{}", x);
+
+            //Find variations
+            let variations = find_variants(&graph, &reference);
+
+            //Write variations to file
+            write_to_file(&PathBuf::from(out_path), &variations).unwrap();
+        } else {
+            panic!("Couldn't parse gfa file!");
+        }
     } else {
-        panic!("Couldn't parse test GFA file!");
+        panic!("Couldn't parse reference file!");
     }
-
 }
 
-fn find_variants(graph: &HashGraph) -> Vec<Variant> {
+fn build_graph(gfa: &GFA) -> Graph<&str, &str> {
+    let mut graph = Graph::<&str, &str>::new();
+
+    let mut nodes_id = vec![];
+
+    for seg in &gfa.segments {
+        let id = graph.add_node(&seg.name);
+        nodes_id.push(id);
+    }
+
+    let mut edges = vec![];
+    for link in &gfa.links {
+        let e = (&link.from_segment, &link.to_segment);
+        edges.push(e);
+    }
+
+    println!("{:?}",edges);
+
+    graph
+}
+
+fn find_variants(graph: &Graph<&str, &str>, reference: &String) -> Vec<Variant> {
+    
     // TODO: function
     let mut variations: Vec<Variant> = Vec::new();
     
     //is this correct?
-    let first_node = graph.get_handle(NodeId::from(1), false);
+    //let first_node = graph.get_handle(NodeId::from(1), false);
 
     //traverse the graph
     //graph.for_each_handle(|h| {
@@ -74,6 +123,19 @@ fn find_variants(graph: &HashGraph) -> Vec<Variant> {
     //);
 
     variations
+}
+
+fn parse_reference(path: &PathBuf) -> Option<String> {
+    let file = File::open(path).expect(&format!("Error opening file {:?}", path));
+    
+    let reader = BufReader::new(file);
+    let mut reference = String::new();
+
+    for line in reader.lines() {
+        reference.push_str(&line.unwrap());
+    }
+
+    Some(reference)
 }
 
 fn write_to_file(path: &PathBuf, variations: &Vec<Variant>) -> std::io::Result<()> {
