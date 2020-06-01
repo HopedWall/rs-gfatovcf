83e19a4d93e2c81e6fa1713776e147872b4617a1
HopedWall <francesco.porto97@gmail.com>
2020-06-01 18:09:12 +0200

    Continued implementing GFAtoVCF

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::collections::HashMap;
+//use std::collections::BTreeMap; //like hashmap but sorted
 use std::path::PathBuf;
 use handlegraph::graph::HashGraph;
 use handlegraph::handlegraph::HandleGraph;
-use handlegraph::handle::{NodeId,Handle};
+use handlegraph::handle::{NodeId,Handle, Direction};
 use handlegraph::pathgraph::PathHandleGraph;
 use gfa::gfa::GFA;
 use std::fs::File;
@@ -12,8 +13,9 @@ extern crate clap;
 extern crate chrono;
 use chrono::Utc;
 use std::io::{BufReader,BufRead};
-use petgraph::prelude::*;
-
+use petgraph::Graph;
+use std::collections::VecDeque;
+use std::collections::HashSet;
 
 
 struct Variant {
@@ -28,12 +30,14 @@ struct Variant {
 }
 
 // Why doesn't this work?
-fn process_step(g : HashGraph, s : HashGraph::StepHandle) -> String {
+fn process_step(g : &HashGraph, s : &HashGraph::StepHandle) -> String {
     let h = g.get_handle_of_step(s);
     let is_rev = h.is_reverse();
     let id = h.id();
     
+    // Should be converted to u64 and then String?
     let result = String::from(id);
+    
     if is_rev == true {
         result.push_str("+");
     } else {
@@ -43,7 +47,7 @@ fn process_step(g : HashGraph, s : HashGraph::StepHandle) -> String {
     result
 }
 
-fn create_into_hashmap(g : HashGraph, path_to_steps : HashMap<String, Vec<String>>,  path : HashGraph::PathHandle, step : HashGraph::StepHandle) {
+fn create_into_hashmap(g : &HashGraph, path_to_steps : &HashMap<String, Vec<String>>,  path : &HashGraph::PathHandle, step : &HashGraph::StepHandle) {
     let path_name = g.get_path_name(path);
     if !path_to_steps.contains_key(path_name) {
         path_to_steps[path_name] = Vec::new();
@@ -51,19 +55,62 @@ fn create_into_hashmap(g : HashGraph, path_to_steps : HashMap<String, Vec<String
     path_to_steps[path_name].push(process_step(g,step));
 }
 
+// ------------ FROM NOW ON -- ODGI REQUIRED? ---------
+
 // What's the fourth parameter for?
-fn create_edge_and_so_on(g : HashGraph, g_dfs : Vec<String>, handle1 : HashGraph::NodeHandle, handle2 : HashGraph::NodeHandle, so_on_function : &dyn Fn() -> i32, args : String) {
-    let handle1_id = g.get_id(handle1);
-    let handle2_id = g.get_id(handle2); 
+// fn create_edge_and_so_on(g : HashGraph, g_dfs : Vec<String>, handle1 : HashGraph::NodeHandle, handle2 : HashGraph::NodeHandle, so_on_function : &dyn Fn() -> i32, args : Fn()) {
+//     let handle1_id = g.get_id(handle1);
+//     let handle2_id = g.get_id(handle2); 
 
-    if !g_dfs.contains(handle1) {
-        g_dfs.append(g.get_sequence(handle1));
-    }
+//     if !g_dfs.contains(handle1) {
+//         g_dfs.append(g.get_sequence(handle1));
+//     }
 
-    if !g_dfs.contains(handle2) {
-        g_dfs.append(g.get_sequence(&handle2));
-    }
-}
+//     if !g_dfs.contains(handle2) {
+//         g_dfs.append(g.get_sequence(&handle2));
+//     }
+// } //TODO: understand this function better
+
+// fn dfs(g : HashGraph, node_id : HashGraph::NodeHandle) {
+//     let current_node = g.get_handle(node_id, false);
+//     let sequence_node = g.get_sequence(&current_node);
+
+//     g.follow_edges(
+//         &current_node, 
+//         Direction::Right,  //What should go here?
+//         |neighbor| {
+//             //create_edge_and_so_on(g, g_dfs: Vec<String>, current_node, neighbor, dfs, g.get_id(neighbor));
+//             true
+//         });
+// }
+
+// fn calculate_distance(visited_node_id_set : HashSet<String>, prev_node_id : String, neighbour_id : String, Q : VecDeque<String>, distances_map : HashMap<String,u64>) {
+//     if !visited_node_id_set.contains(&neighbour_id) {
+//         distances_map[&neighbour_id] = distances_map[&prev_node_id] + 1;
+//         // is push_back correct?
+//         Q.push_back(neighbour_id);
+//         visited_node_id_set.insert(neighbour_id);
+//     }
+// }
+
+// // This function can be optimized via closures!
+// fn bfs_distances(g : HashGraph, starting_node_id : HashGraph::NodeHandle) {
+//     let visited_node_id_set = HashSet::new();
+//     let node_id_list = Vec::new();
+//     g.for_each_handle(|h| true);
+
+//     for node_id in node_id_list {
+//         distances_map[node_id] = 0;
+//     }
+//     node_id_list.clear();
+
+//     //TODO: complete this function
+// }
+
+// // fn show_edge(g_dfs : Graph, a : u64, b : u64) {
+    
+// //     print!(g_dfs.get_id)
+// // }
 
 
 
@@ -112,7 +159,7 @@ fn main() {
         let path_to_steps_map = HashMap::new();
 
         let get_path_to_steps = |p| {
-            graph.for_each_step_in_path(p, |s| create_into_hashmap(graph, path_to_steps_map, p, s))
+            graph.for_each_step_in_path(p, |s| create_into_hashmap(&graph, &path_to_steps_map, &p, &s))
         };
 
         graph.for_each_handle(get_path_to_steps);    
@@ -144,6 +191,7 @@ fn main() {
         }
 
         // TODO: this must be sorted
+        //sorted = new BTreeMap(node_id_to_path_and_pos_map);
         for node_id in node_id_to_path_and_pos_map.keys() {
             let path_and_pos_map = node_id_to_path_and_pos_map[node_id];
             println!("Node_id : {}", node_id);
@@ -156,8 +204,10 @@ fn main() {
         let start_node = graph.get_handle(NodeId::from(1), false);
 
         //let g_dfs = Vec::new(); //Will be used a stack for DFS
-        // Not an array as comment states, but a graph/tree
-        //let g_dfs = Petgraph::new();
+        //Not an array as comment states, but a graph/tree
+        //let g_dfs = petgraph::Graph::new();
+
+        //dfs(graph,1);
 
         //Find variations
         //let variations = find_variants(&graph, &reference);
